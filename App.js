0992a0ba7d7de37488367ad666d5f55465a9764b
leedd87 <leedav87@gmail.com>
2024-01-23 10:01:51 -0300

    testing text color in expo

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Coder! y mas</Text>
+      <Text style={{ color: 'blue' }}>Hola Coder! y mas</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,7 +13,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'red',
     alignItems: 'center',
     justifyContent: 'center',
   },
