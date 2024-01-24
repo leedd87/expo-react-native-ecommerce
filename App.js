98220ec6e71c2c209dfcc806a6ca55d603ead09e
leedd87 <leedav87@gmail.com>
2024-01-24 20:16:17 -0300

    changes made during after 1

--- App.js
+++ App.js
@@ -8,8 +8,8 @@ export default function App() {
       <TouchableOpacity onPress={() => console.log('PRESS IT')} style={{ backgroundColor: 'red' }}>
         <Text>TOUCH ME!</Text>
       </TouchableOpacity>
-      <View style={{ justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: 'black', borderRadius: 30 }}>
-        <Text>Produdct 1</Text>
+      <View style={{ justifyContent: 'center', alignItems: 'center', borderWidth: 1, borderColor: 'black', borderRadius: 10, height: 200, width: 300 }}>
+        <Text style={[styles.backgroundColor, { color: 'yellow' }]}>Produdct 1</Text>
         <Text>Produdct 2</Text>
         <Text>Produdct 3</Text>
         <Text>Produdct 4</Text>
@@ -24,8 +24,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'red',
+    backgroundColor: 'gray',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  textContainer: {
+    backgroundColor: 'blue'
+  }
 });
