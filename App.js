8de08c5fe70fc7387fc98189bf875a77cb9f7cc2
leedd87 <leedav87@gmail.com>
2024-01-23 22:59:34 -0300

    testing react-native components

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, TouchableOpacity, View } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text style={{ color: 'blue' }}>Hola Coder! y mas</Text>
+      <TouchableOpacity onPress={() => console.log('PRESS IT')} style={{ backgroundColor: 'red' }}>
+        <Text>TOUCH ME!</Text>
+
+      </TouchableOpacity>
       <StatusBar style="auto" />
     </View>
   );
